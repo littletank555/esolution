0514a12b44c16c45c8cceb27a3e85718a2cbbf8d
littletank555 <1010042778@qq.com>
2020-05-14 14:43:23 +0800

    init

--- .history/src/api/excel_20200120163645.js
+++
@@ -1,9 +0,0 @@
-export function created_quotation_excel(values) {//報價單
-    return request({
-        url: "admin-index?page=exportexcel&action=quotation-excel",
-        method: "POST",
-        data: Qs.stringify({
-            ...values
-        })
-    })
-}
--- .history/src/utils/token_20191218104448.js
+++
@@ -1,5 +0,0 @@
-import md5 from "js-md5";
-import moment from "moment";
-export function convertToToken(module, action) {
-    return md5('dc2cfad1400d23c512e63a028f54b0b2' + module + action + moment.utc().format("YYYY-MM-DD"));
-}
